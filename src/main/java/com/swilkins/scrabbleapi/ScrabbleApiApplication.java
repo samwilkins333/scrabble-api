1ad77edd184fbf684d24f7089cf6d21fd09c2483
Sam Wilkins <samwilkins333@gmail.com>
2020-08-01 15:09:31 -0700

    initial commit

---
+++ src/main/java/com/swilkins/scrabbleapi/ScrabbleApiApplication.java
@@ -0,0 +1,13 @@
+package com.swilkins.scrabbleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrabbleApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ScrabbleApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/swilkins/scrabbleapi/ScrabbleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swilkins.scrabbleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrabbleApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
