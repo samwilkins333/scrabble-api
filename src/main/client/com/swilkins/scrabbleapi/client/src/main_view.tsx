b8db305d831648e4a2dc20676bb8ae4493da71f1
Sam Wilkins <samwilkins333@gmail.com>
2020-08-03 00:22:55 -0700

    Fundamentals of api sandbox in place

--- src/main/client/com/swilkins/scrabbleapi/client/src/main_view.tsx
+++ src/main/client/com/swilkins/scrabbleapi/client/src/main_view.tsx
@@ -2,20 +2,28 @@
 import * as React from "react";
 import "./main_view.scss";
 import { observer } from "mobx-react";
+import { observable, runInAction, action } from "mobx";
 import {Server} from "./utilities";
 
 const dimensions = 15;
 
+interface Cell { 
+    active: boolean,
+    ref: React.RefObject<HTMLInputElement>
+}
+
 @observer
 export default class MainView extends React.Component {
-    private cellArray: React.RefObject<HTMLInputElement>[][] = [];
+    @observable.deep private cellArray: Cell[][] = [];
+    @observable private candidates: string[] = [];
+    private temporary: { x: number, y: number }[] = [];
 
     constructor(props: {}) {
         super(props);
         for (let y = 0; y < dimensions; y++) {
-            const row: React.RefObject<HTMLInputElement>[] = [];
+            const row: Cell[] = [];
             for (let x = 0; x < dimensions; x++) {
-                row.push(React.createRef());
+                row.push({ active: false, ref: React.createRef() });
             }
             this.cellArray.push(row);
         }
@@ -26,43 +34,79 @@ export default class MainView extends React.Component {
         for (let y = 0; y < dimensions; y++) {
             const row: JSX.Element[] = [];
             for (let x = 0; x < dimensions; x++) {
+                const { active, ref } = this.cellArray[y][x];
                 row.push(
                     <div className={"flex"}>
                         <input
-                            ref={this.cellArray[y][x]}
+                            ref={ref}
                             maxLength={1}
                             className={"cell"}
+                            style={{
+                                backgroundColor: active ? "red" : "white"
+                            }}
+                            onChange={action((e: React.ChangeEvent<HTMLInputElement>) => {
+                                if (!(this.cellArray[y][x].active = /^[a-z]$/.test(e.target.value.toLowerCase()))) {
+                                    e.target.value = "";
+                                }
+                            })}
                         />
                     </div>
                 )
             }
             collector.push(<div className={"flex row"}>{...row}</div>)
         }
-        return collector;
+        return <div>{...collector}</div>;
     }
 
-    private logBoard = async () => {
+    private submitBoard = async () => {
         const board: { row: number, tiles: string  }[] = [];
         for (let y = 0; y < dimensions; y++) {
             const row: string[] = [];
             for (let x = 0; x < dimensions; x++) {
-                row.push(this.cellArray[y][x].current?.value || "-");
+                row.push(this.cellArray[y][x].ref.current?.value || "-");
             }
             board.push({ row: y, tiles: row.join("") });
         }
-        console.log((await Server.Post("/api/generate", {
-            board,
-            rack: "a*febji"
-        })).candidates[0]);
+        const rack = "a*febji";
+        const { candidates } = await Server.Post("/api/generate", { board, rack });
+        runInAction(() => this.candidates = candidates);
     };
 
     render() {
         return (
-            <div className={"main-container flex col centering"}>
-                {this.cells}
+            <div className={"ssf w100 h100 flex col centering"}>
+                <div className={"flex h50 w100 flexgrow1"}>
+                    {this.cells}
+                    <div className={"flex col scroll"}>
+                        {this.candidates.map(candidate => (
+                            <span
+                                onClick={() => {
+                                    for (const { x, y } of this.temporary) {
+                                        const element = this.cellArray[y][x].ref.current;
+                                        element && (element.value = "");
+                                    }
+                                    this.temporary = [];
+                                    const matches = /\[([^\[\]]+)\]/.exec(candidate)!;
+                                    const placementsRaw = matches[1];
+                                    let placementMatch: RegExpExecArray | null;
+                                    const regex = /([a-z\*\{\}]+)\((\d+), (\d+)\)/g;
+                                    while ((placementMatch = regex.exec(placementsRaw))) {
+                                        const x = Number(placementMatch[2]);
+                                        const y = Number(placementMatch[3]);
+                                        const element = this.cellArray[y][x].ref.current;
+                                        if (element) {
+                                            element.value = placementMatch[1].replace(/[\{*\}]+/g, "");
+                                            this.temporary.push({ x, y, });
+                                        }
+                                    }
+                                }}
+                            >{candidate}</span>
+                        ))}
+                    </div>
+                </div>
                 <button
                     className={"pat20"}
-                    onClick={this.logBoard}
+                    onClick={this.submitBoard}
                 >Submit</button>
             </div>
         );
