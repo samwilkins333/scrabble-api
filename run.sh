b567a7e07b18fc364e01ca2169f60241121c3ea7
Sam Wilkins <samwilkins333@gmail.com>
2020-09-02 02:28:41 -0700

    Removed run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd src/main/java
-javac -g -cp ".:/Users/swilkinss2012/.m2/repository/org/springframework/boot/spring-boot-autoconfigure/2.3.2.RELEASE/spring-boot-autoconfigure-2.3.2.RELEASE.jar" com/swilkins/scrabbleapi/ScrabbleApiApplication.java;
-java com/swilkins/scrabbleapi/ScrabbleApiApplication 10;
