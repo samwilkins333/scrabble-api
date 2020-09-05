fe1f3bfb8a9743dd787446c389e29f082ebe5229
Sam Wilkins <samwilkins333@gmail.com>
2020-09-05 14:20:44 -0700

    Restructuring to combine react with js

--- src/main/client/com/swilkins/scrabbleapi/client/src/index.tsx
+++ src/main/client/com/swilkins/scrabbleapi/client/src/index.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import MainView from "./main_view";
+// import MainView from "./main_view";
 
-ReactDOM.render(<MainView/>, document.getElementById("root"));
+// declare const editor: any;
+
+ReactDOM.render(<div>Hello there!</div>, document.getElementById("root"));
